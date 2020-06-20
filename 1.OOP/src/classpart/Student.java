e8aa2a4f81c0a9aa3b591f07ad649a5630bdb1e8
Milky <tpdhks0369@gmail.com>
2020-06-21 03:59:55 +0900

    feat : 객체지향 프로그래밍과 클래스
    fastcampus01, 02 내용

---
+++ 1.OOP/src/classpart/Student.java
@@ -0,0 +1,16 @@
+package classpart;
+
+// public 키워드 클래스는 작업 파일마다 1개가 유일해야 한다.
+public class Student {
+	// class는 객체를 코드화 한 것.
+	// class는 멤버변수와 메서드로 구성 할 수 있다.
+	
+	int studentId;
+	String studentName;
+	String address;
+	
+	
+	public void showStudentInfo() { // 매개변수 영역
+		System.out.println(studentName + ", " + address);
+	}
+}
---
+++ 1.OOP/src/classpart/StudentTest.java
@@ -0,0 +1,13 @@
+package classpart;
+
+public class StudentTest {
+	
+	public static void main(String[] args) {
+		Student student = new Student();
+		student.studentName = "양세완";
+		student.address = "서울";
+		
+		student.showStudentInfo();
+	}
+
+}
