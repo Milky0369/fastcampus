43b4bd2156f127ca60bbeec041a071b9c3713b87
Milky <tpdhks0369@gmail.com>
2020-07-02 22:02:54 +0900

    feat : 객체지향 프로그래밍과 클래스
    fastcampus29 내용

---
+++ 1.OOP/src/polymorphism/Customer.java
@@ -0,0 +1,63 @@
+package polymorphism;
+
+public class Customer {
+	
+	protected int custId;
+	protected String custName;
+	protected String custGrade;
+	protected int bonusPoint;
+	protected double bonusRatio;
+	
+//	public Customer() {
+//		custGrade = "SILVER";
+//		bonusRatio = 0.01;
+//		
+//		System.out.println("Customer() 생성자 호출");
+//	}
+	
+	public Customer(int custId, String custName) {
+		this.custId = custId;
+		this.custName = custName;
+		
+		custGrade = "SILVER";
+		bonusRatio = 0.01;
+		
+		System.out.println("Customer() 생성자 호출");
+	}
+	
+	public int calcPrice(int price) {
+		bonusPoint += price * bonusRatio;
+		return price;
+	}
+	
+	public String showCustomerInfo() {
+		return custName + "님의 등급은 " + custGrade + "이며 적립된 보너스 포인트는 " + bonusPoint + "점 입니다.";
+	}
+
+	public int getCustId() {
+		return custId;
+	}
+
+	public void setCustId(int custId) {
+		this.custId = custId;
+	}
+
+	public String getCustName() {
+		return custName;
+	}
+
+	public void setCustName(String custName) {
+		this.custName = custName;
+	}
+
+	public double getBonusRatio() {
+		return bonusRatio;
+	}
+
+	public void setBonusRatio(double bonusRatio) {
+		this.bonusRatio = bonusRatio;
+	}
+	
+	
+	
+}
---
+++ 1.OOP/src/polymorphism/VIPCustomer.java
@@ -0,0 +1,35 @@
+package polymorphism;
+
+public class VIPCustomer extends Customer{
+
+	double salesRatio;
+	private int agentId;
+	
+//	public VIPCustomer() {
+//		
+//		super(0, null);
+//		
+//		custGrade = "VIP";
+//		bonusRatio = 0.05;
+//		salesRatio = 0.1;
+//		
+//		System.out.println("VIPCustomer() 생성자 호출");
+//	}
+	
+	@Override
+	public int calcPrice(int price) {
+		bonusPoint += price * bonusRatio;
+		return price - (int)(price * salesRatio);
+	}
+	
+	public VIPCustomer(int custId, String custName) {
+		super(custId, custName);
+		
+		custGrade = "VIP";
+		bonusRatio = 0.05;
+		salesRatio = 0.1;
+		
+		System.out.println("VIPCustomer() 생성자 호출");
+	}
+	
+}
