3ad07c95f2bd3b5f6bc637f4fff5a02401e7444e
Milky <tpdhks0369@gmail.com>
2020-08-05 22:49:10 +0900

    feat : 객체지향 프로그래밍과 클래스
    fastcampus47 내용

---
+++ 1.OOP/src/classex/MyDate.java
@@ -0,0 +1,24 @@
+package classex;
+
+import java.util.Objects;
+
+public class MyDate {
+
+    int day;
+    int month;
+    int year;
+
+    public MyDate(int day, int month, int year){
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(day, month, year);
+    }
+
+}
+
+
